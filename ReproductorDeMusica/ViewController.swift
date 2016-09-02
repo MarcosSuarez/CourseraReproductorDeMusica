0bccf173369cdc1d84d809d2d60e99d15c1f96d0
Marcos <marcos.suarez@rocketmail.com>
2016-09-02 23:20:57 +0200

    pequeñas correcciones

--- ReproductorDeMusica/ViewController.swift
+++ ReproductorDeMusica/ViewController.swift
@@ -12,12 +12,10 @@ import AVFoundation
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var labelTitulo: UILabel!
-    
     @IBOutlet weak var imagen: UIImageView!
-    
     @IBOutlet weak var ruleta: UIPickerView!
     
-    let nombreCanciones = ["primera","segunda","tercera","cuarta","quinta","sexta"]
+    let album = ["primera","segunda","tercera","cuarta","quinta","sexta","septima","octava","novena","decima"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,16 +32,19 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         // Cantidad de Filas.
-        return nombreCanciones.count
+        return album.count
     }
     
     func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         // Entrega String dependiendo de la fila y columna.
-        return nombreCanciones[row]
+        return album[row]
     }
     
-    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        print("Canción seleccionada: \(nombreCanciones[row])")
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        // Se ha seleccionado una fila en el pickerView.
+        labelTitulo.text = album[row]
+        // Iniciar la canción
     }
     
     // MARK: VOLUMEN
@@ -68,17 +69,17 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     {
         let posicionActual = ruleta.selectedRowInComponent(0)
         
-        var azar = Int(arc4random_uniform(UInt32(nombreCanciones.count)))
+        var azar = Int(arc4random_uniform(UInt32(album.count)))
         
         // Para minimizar las opciones que se repitan.
-        if posicionActual == azar { azar = Int(arc4random_uniform(UInt32(nombreCanciones.count))) }
+        if posicionActual == azar { azar = Int(arc4random_uniform(UInt32(album.count))) }
         
         ruleta.selectRow(azar, inComponent: 0, animated: true)
         
         // PLAY DE LA CANCIÓN:
-        print("Canción Nro. \(azar+1) sonando: \(nombreCanciones[azar])")
+        print("Canción Nro. \(azar+1) sonando: \(album[azar])")
         // cambio el título.
-        labelTitulo.text = nombreCanciones[azar]
+        labelTitulo.text = album[azar]
         // Muestro el Albúm.
         
         // Suena la canción.
