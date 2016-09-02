b578462422e724697db1d4913d5e069cae0d4168
Marcos <marcos.suarez@rocketmail.com>
2016-09-02 16:43:44 +0200

    Operatividad del PickerView

--- ReproductorDeMusica/ViewController.swift
+++ ReproductorDeMusica/ViewController.swift
@@ -8,35 +8,63 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var labelTitulo: UILabel!
     
     @IBOutlet weak var imagen: UIImageView!
     
+    @IBOutlet weak var ruleta: UIPickerView!
+    
+    let nombreCanciones = ["primera","segunda","tercera","cuarta","quinta","sexta"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func volumen(sender: UISlider) {
+        
+        ruleta.delegate = self
     }
     
+    //MARK: PickerView RULETA
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        // Cantidad de Columnas.
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        // Cantidad de Filas.
+        return nombreCanciones.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        // Entrega String dependiendo de la fila y columna.
+        return nombreCanciones[row]
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Canción seleccionada: \(nombreCanciones[row])")
+    }
+
+    // MARK: VOLUMEN
+    @IBAction func volumen(sender: UISlider) {
+        print("volumen en \(sender.value * 100)%")
+    }
+    
+    // MARK: BOTONES
     @IBAction func detener(sender: UIButton) {
+        print("presionado DETENER")
     }
 
     @IBAction func tocar(sender: UIButton) {
+        print("presionado TOCAR")
     }
     
     @IBAction func pausar(sender: UIButton) {
+        print("Presionado PAUSAR")
     }
     
     @IBAction func shufle(sender: UIButton) {
+        print("presionado SHUFLE")
     }
     
 }
